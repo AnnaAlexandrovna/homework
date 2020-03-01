b9273e8e11324950bbb7801ab2d506a290ce4100
Anna <salatik339@mail.ru>
2020-03-02 01:11:47 +0300

    k

---
+++ src/main/java/k1/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package k1;
+
+public class ClassicalMusic implements  Music {
+    @Override
+    public String getSong() {
+        return "Classical song";
+    }
+}
---
+++ src/main/java/k1/RockMusic.java
@@ -0,0 +1,8 @@
+package k1;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Rock song";
+    }
+}
